e85d5bd415955e72a25e3dfab2a6a4be65aa1e33
Onur Ersel <onur@glomo.se>
2018-09-23 02:15:34 +0200

    Added more platforms

--- index.js
+++ index.js
@@ -2,13 +2,39 @@
 
 const axios = require('axios');
 const fs = require('fs');
+const program = require('commander');
 
-axios.get('https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore')
-  .then(response => {
-    const data = `.DS_Store\n.idea\n\n${response.data}`;
-    fs.writeFileSync('.gitignore', data, {encoding: 'utf8'});
-    console.log("Created .gitignore file for node.js project");
-  })
-  .catch(err => {
-    console.log(err);
-  })
+const URLS = {
+  NODE: 'https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore',
+  SWIFT: 'https://raw.githubusercontent.com/github/gitignore/master/Swift.gitignore',
+  OBJC: 'https://raw.githubusercontent.com/github/gitignore/master/Objective-C.gitignore',
+}
+
+function create(url, projectTypeName) {
+  return axios.get(url)
+    .then(response => {
+      const data = `.DS_Store\n.idea\n\n${response.data}`;
+      fs.writeFileSync('.gitignore', data, {encoding: 'utf8'});
+      console.log(`Created .gitignore file for ${projectTypeName} project`);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+program
+  .version('1.0.1', )
+  .option('--node')
+  .option('--swift')
+  .option('--objc')
+  .parse(process.argv);
+
+if (program.node) {
+  create(URLS.NODE, 'node.js');
+} else if (program.swift) {
+  create(URLS.SWIFT, 'Swift');
+} else if (program.objc) {
+  create(URLS.OBJC, 'Objective C');
+} else {
+  console.log('No project type selected. Use `create-gitignore --help` for options.')
+}
